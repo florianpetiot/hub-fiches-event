a67bfec6a30adc01b3833f82e62245e00eb65a4b
Florian <florian.petiot44@gmail.com>
2026-02-21 17:12:33 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { supabase } from '$lib/supabase'
+  
+  async function test() {
+    const { data, error } = await supabase.from('ma_table').select('*')
+    console.log(data, error)
+  }
+</script>
+
+<button onclick={test}>Tester Supabase</button>
