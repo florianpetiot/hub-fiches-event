606fdf29f3b7ccbaeac16bd2de2067d363247f7c
Florian <florian.petiot44@gmail.com>
2026-02-23 17:33:48 +0100

    feat: ajouter une page d'erreur personnalisée avec gestion des statuts 404 et 403

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,5 +1,15 @@
+import { createBrowserClient, isBrowser } from '@supabase/ssr'
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
 import type { LayoutLoad } from './$types'
 
-export const load: LayoutLoad = async ({ data, parent }) => {
-  return { session: data.session }
+export const load: LayoutLoad = async ({ data, depends, fetch }) => {
+  depends('supabase:auth')
+
+  const supabase = isBrowser()
+    ? createBrowserClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
+        global: { fetch },
+      })
+    : undefined
+
+  return { session: data.session, supabase }
 }
